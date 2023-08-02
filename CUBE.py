881868be8b65436234d8eaa064550d6863cb02f4
Krish Rajchal <67868262+krishrajchal@users.noreply.github.com>
2023-08-02 15:40:39 -0600

    Add files via upload

--- CUBE.py
+++ CUBE.py
@@ -8,15 +8,14 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController as fpc
 from ursina.shaders import basic_lighting_shader as bls
 from ursina.prefabs.trail_renderer import TrailRenderer as tr
-from ursina.prefabs.health_bar import HealthBar as hb
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
 
 application.asset_folder = application.asset_folder.parent
 
 from random import randint as r
 
 import math
-
-from pyautogui import hotkey as ht
 
 app = Ursina()
 
@@ -26,31 +25,12 @@ player=fpc(model='cube', collider='box', texture='white_cube',
     eternal=True
     )
 
-player_health = hb(max_value=100000,
-                   value=100000,
-                   position=(0, -700))
-
-boss_health = hb(max_value=1000,
-                 value=1000)
-
-updater = Entity(pos=(1000000, -100000000, 10000000),
-                 color=color.rgb(0, 0, 0, 0))
-
 do = True
 
-entitys = [i for i in range(1000)]
-
-for i in range(len(entitys)):
-    entitys[i] = Entity(model='sphere', color=color.red,
-                   scale=1,
-                   position=Vec3(r(-1000, 1000), 2, r(-1000, 1000)),
-                   shader=bls)
-
-tri = Entity(model='cone.obj',
-                shader=bls,
-                scale=5,
-                color=color.gray,
-                position=(5, 1, 0))
+point = Entity(model='sphere', color=color.red,
+               scale=1,
+               position=Vec3(1, 2, 0),
+               shader=bls)
 
 def pos3d(x1, y1, z1, x2, y2, z2, speed=.5):
     distance = math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
@@ -61,9 +41,31 @@ def pos3d(x1, y1, z1, x2, y2, z2, speed=.5):
     
     return (x, y, z)
 
+def done():
+    global do
+    if do:
+        text = """
+        Well, you did it! You killed the already easy boss by
+        clicking the boss. Note: This is still not done, I
+        was not able to finish this in time since there was so
+        much to add to this game. 
+        """
+        root = ScrolledText(master=tk.Tk(), height=15, width=35)
+        root.insert("end", text)
+        root.winfo_toplevel().title("Finished")
+        root.config(state='disabled')
+        root.pack(fill='both', side="left", expand=True)
+        root.focus_set()
+        do = False
+
 class game():
     def __init__(self):
         self.num = 2
+        self.tri = Entity(model='cone.obj',
+                     collider='box',
+                     shader=bls,
+                     scale=5,
+                     position=(5, 1, 0))
 
     def clear(self):
         scene.clear()
@@ -73,79 +75,75 @@ class game():
     def rs(self):shape = ['cube', 'cube_uv_top', 'sky_dome', 'sphere'];return shape[r(0, len(shape)-1)]
 
     def shaper(self):
-        for x in range(100):
+        for x in range(1500):
             Entity(model=self.rs(), texture='white_cube',
                    collider='box',
                    color=color.orange,
-                   position=(r(-10000, 10000), .5, r(-10000, 10000)),
+                   position=(r(-1000, 1000), .5, r(-1000, 1000)),
                    rotation=(r(0, 360), r(0, 360), r(0, 360)),
                    scale=(self.num, self.num, self.num),
                    shader=bls)
             self.num = r(2,5)
 
     def room(self, pos, fun):
-        target = Entity(model='cube',
-               scale=100,
-               position=pos,
+        target = Entity(model='sphere',
+                   position=pos)
+
+        # Left side of the house
+        Entity(model='cube',
+               scale=(100, 100, 1),
+               position=(pos[0]+50, pos[1], pos[2]),
                color=color.rgb(257, 257, 257),
                shader=bls,
                collider='box')
+
+        Entity(model='cube',
+               scale=())
         
         def b():
             x = target.x - player.x
             y = target.y - player.y
             z = target.z - player.z
             d = ((x**2) + (y**2) + (z**2)) ** .5
-            if d > 5:
+            if d > 143:
                 fun()
 
         target.update = b
 
-    def boss(self, player_ded, boss_ded):
-        tr(target=tri)
+    def boss(self):
+        tr(target=self.tri)
 
         def face():
-            def sub_health(duration):
-                player_health.value -= 10
-                
             def toInvoke():
-                if distance(tri, player) <= 3.5:
-                    invoke(sub_health, duration=100)
-                    return
+                if distance(self.tri, player) <= 3.5:
+                    return True
                 
-                tri.look_at(player)
-                tri.position = pos3d(tri.x, tri.y, tri.z,
+                self.tri.look_at(player)
+                self.tri.position = pos3d(self.tri.x, self.tri.y, self.tri.z,
                                           player.x, player.y, player.z, 0.27)
-
-            if player_health.value == 0:
-                player_ded()
-
-            if boss_health.value == 0:
-                boss_ded()
                 
             toInvoke()
-            print(mouse.hovered_entity == tri)
+            if toInvoke():
+                done()
+                return
 
-        tri.update = face
+        self.tri.update = face
 
-def collide(i):
-    x = player.x - entitys[i].x
-    y = player.y - entitys[i].y
-    z = player.z - entitys[i].z
+    def input(self, key):
+        if self.tri.hovered:
+            if key == 'left mouse down':
+                done()
+                destroy(self.tri)
+                raise
+                
+
+def updat():
+    x = player.x - point.x
+    y = player.y - point.y
+    z = player.z - point.z
     d = ((x**2) + (y**2) + (z**2)) ** .5
     if d < 2:
-        destroy(entitys[i])
-        player_health.value += 10
-        
-    '''
-    if distance(player.position, entitys[i].position) < 2:
-        destroy(entitys[i])
-        player_health.value += 10
-    '''
-
-def col():
-    for i in range(len(entitys)):
-        collide(i)
+        destroy(point)
 
 def d():
     Entity(model='cube',
@@ -154,26 +152,13 @@ def d():
            color=color.rgb(257, 257, 257),
            shader=bls,
            collider='box')
-
-def good():
-    print('pihfndzb;niujshugbnpaongprboniapiouegpiuadfbpjapiehfoihnadfinboauigfoiuhaoignendraobadorgood')
-    ht('alt', 'f4')
-
-def bad():
-    print('############################################################################################')
-    ht('alt', 'f4')
-
-def input(key):
-    if mouse.hovered_entity == tri:
-        if mouse.left == True:
-            boss_health.value -= 10
         
 a = game()
-#a.shaper()
-#a.boss(bad, good)
+#a.room(Vec3(0, 0, 0), d)
+a.shaper()
+a.boss()
 
-#for i in range(len(entitys)):
-#    entitys[i].update = col
+point.update = updat
 
 window.title = "CUBE"
 window.borderless = False
@@ -182,10 +167,11 @@ window.vsync = False
 Entity(model='plane', collider='box',
        color=color.blue,
        texture='white_cube',
-       texture_scale=(10000,10000),
-       scale=(10000,1,10000),
+       texture_scale=(1000,1000),
+       scale=(1000,1,1000),
        eternal=True)
 
 Sky()
 
 app.run()
+
